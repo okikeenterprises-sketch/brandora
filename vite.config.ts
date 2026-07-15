9b5e65651168898635f9b643b95f770534f00c39
A2-DIGIHUB <a2digihub@gmail.com>
2026-07-15 15:27:39 +0100

    switched from wrangler to tanstack

--- vite.config.ts
+++ vite.config.ts
@@ -2,23 +2,19 @@ import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
-import { cloudflare } from "@cloudflare/vite-plugin";
 
-export default defineConfig(({ mode }) => {
-  return {
-    plugins: [
-      tsconfigPaths(),
-      tailwindcss(),
-      tanstackStart({
-        server: { entry: "server" },
-      }),
-      mode === "production" ? cloudflare() : null,
-    ].filter(Boolean),
-    resolve: {
-      alias: {
-        "@": "/src",
-      },
+export default defineConfig({
+  plugins: [
+    tsconfigPaths(),
+    tailwindcss(),
+    tanstackStart({
+      server: { entry: "server" },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": "/src",
     },
-  };
+  },
 });
 
